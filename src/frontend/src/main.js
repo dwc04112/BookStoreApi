40e45aa385f8fdd5038592486eb9785174bb65b8
dwc04112 <dwc04112@gmail.com>
2022-02-09 18:23:20 +0900

    npm install [] --save 1. axios 2. vuetify 3. vue-router 4. vuex
    main.js에 import

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue';
+import axios from "./plugins/axios";
+import vuetify from './plugins/vuetify'
+
+Vue.prototype.$eventBus = new Vue();
+Vue.prototype.$axios = axios
+// 다른 컴포넌트에서는 import 없이 this.$axios로 사용가능
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
