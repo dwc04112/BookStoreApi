53cfeee5f7bed03bc4befd3af758679de38c04e9
dwc04112 <dwc04112@gmail.com>
2022-03-19 04:22:52 +0900

    infomain / infoNavi 생성

---
+++ src/frontend/src/views/mypage/InfoMain.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "mypageMain"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/frontend/src/views/mypage/InfoNavi.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "InfoNavi"
+}
+</script>
+
+<style scoped>
+
+</style>
