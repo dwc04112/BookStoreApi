8f30a7a2fd332bd0a9daa4033cd22fdf59956df0
dwc04112 <dwc04112@gmail.com>
2022-04-11 01:17:29 +0900

    exam : About Vue 에서 사용할 디자인 DetailView : 책 상새보기 화면 임시 디자인

--- src/frontend/src/views/book/DetailView.vue
+++ src/frontend/src/views/book/DetailView.vue
@@ -1,23 +1,115 @@
 <template>
-  <div>
-    Detail View {{bid}}
-    <div>
-      {{bookData.bookTitle}}
-      <v-img width="200px" :src="bookData.bookThumb"></v-img>
-    </div>
-    <div>
-      <v-chip
-          class="ml-2"
-          v-for="(data, index) in keyword"
-          :key="index"
-          :value="data"
-          outlined
-          small
-          color="rgb(20,20,20)"
-      >{{data}}
-      </v-chip>
-    </div>
-  </div>
+  <v-container fluid class="container">
+    <v-row justify="center" align="center">
+      <!-- 여백 -->
+      <v-col cols="12">
+        <div style="height: 100px"/>
+      </v-col>
+
+
+      <v-col cols="12" sm="12" md="9" lg="7" style="background-color: rgb(220,220,220); height: 3000px">
+        <v-row>
+            <!-- 좌측 책 이미지 & 메뉴-->
+              <v-col cols="12" sm="3" md="2" style="background-color: #b0bec5; height: 3000px;" class="left-section">
+                <div class="align-center flex-column d-flex">
+                  <v-img width="120px" class="left-section" :src="bookData.bookThumb"></v-img>
+                </div>
+
+                <!-- 키워드 -->
+                <div class="mt-4">
+                  <v-chip
+                      class="ma-1"
+                      v-for="(data, index) in keyword"
+                      :key="index"
+                      :value="data"
+                      outlined
+                      small
+                      color="rgb(20,20,20)"
+                  >{{data}}
+                  </v-chip>
+                </div>
+
+
+                <!-- 좌측 메뉴 리스트 -->
+                <v-list
+                    nav
+                    dense
+                    color="#b0bec5"
+                    class="mt-4"
+
+                >
+                  <v-list-item-group
+                      color="primary"
+                  >
+                    <v-list-item
+                        v-for="(item, i) in items"
+                        :key="i"
+
+                    >
+                      <v-list-item-content>
+                        <v-list-item-title><b class="list-text">{{item.text}}</b></v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-list-item-group>
+                </v-list>
+
+              </v-col>
+
+              <v-col cols="12" sm="9" md="9">
+
+                <div class=" align-center flex-column d-flex">
+                  <v-img width="120px" class="main-img" :src="bookData.bookThumb"></v-img>
+                </div>
+
+                {{bookData.bookTitle}}
+                <br>
+                {{bookData.bookAuthor}} | {{bookData.bookPublisher}} | {{bookData.bookPublishedDate}} 출간
+                <br>
+                판매가격 : {{bookData.bookSalePrice}} 원
+                <br>
+
+                <div class="mb-4">
+                  <v-btn>
+                    구매하기
+                  </v-btn>
+                  <v-btn>
+                    장바구니
+                  </v-btn>
+                  <v-btn>
+                    위시리스트
+                  </v-btn>
+                </div>
+                <v-divider/>
+
+                <div class="mt-4 mb-4">
+                  <a>책 소개</a>
+                  <br>
+                  {{bookData.bookContent}}
+                </div>
+
+                <br>
+
+                <div class="mt-4 mb-4">
+                  <a>목차</a>
+                  <br>
+                  {{bookData.bookIndex}}
+                </div>
+
+                <br>
+
+                <div class="mt-4 mb-4">
+                  <a>출판사 서평</a>
+                  <br>
+                  {{bookData.bookPreview}}
+                </div>
+
+              </v-col>
+            </v-row>
+
+
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -26,9 +118,20 @@ export default {
   name: "DetailView",
   data: function (){
     return{
+      //책 정보
       bid: this.$route.query.bid,
       bookData : [],
       keyword : '',
+
+      //좌측 리스트
+      items: [
+        { text: '맨 위로'},
+        { text: '책 정보'},
+        { text: '책 소개'},
+        { text: '목차'},
+        { text: '출판사 서평' },
+        { text: '리뷰'},
+      ],
     }
   },
 
@@ -46,11 +149,43 @@ export default {
   },
   mounted() {
     this.getBookDetail()
+    window.scrollTo(0, 0);
+
   }
 }
 
 </script>
 
 <style scoped>
+.container{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-content: center;
+  margin: 0;
+  padding: 0;
+}
+.list-text{
+  font-weight: bold;
+  font-size: 13px;
+}
+
+.main-img{
+  display: none;
+  position: absolute;
+}
+
+@media screen and (max-width: 768px){
+  .left-section{
+    display: none;
+    position: absolute;
+  }
+  .main-img{
+    display: block;
+    position: relative;
+  }
+
+}
 
 </style>
--- src/frontend/src/views/exam.vue
+++ src/frontend/src/views/exam.vue
@@ -1,29 +1,177 @@
 <template>
+  <v-container fluid style="padding: 0">
+    <v-row>
 
-    <div class="bg">
-      <video
-          muted
-          autoplay
-          loop
-      >
-        <source :src="rain" type="video/mp4">
-      </video>
+        <v-col>
+          <div class="pl-12">
+            <div
+                style="display: inline-block;"
+                class="pa-2"
+                v-for="(book, index) in bookDatas"
+                :key="index"
+            >
+              <v-card
+                  width="160px"
+                  elevation="2"
+                  color="rgba(244, 223, 186, 0.5)"
+              >
+                <v-img
+                    :src="book.bookThumb"
+                    width="160px"
+                    @click.stop="openInfo(book)"
+                ></v-img>
 
-      <div class="text">
-        <p>Lorem Ipsum Dolor</p>
-      </div>
-    </div>
+              </v-card>
+            </div>
+          </div>
+        </v-col>
 
+
+
+      <transition name="sub-slide" mode="in-out">
+        <v-col
+            cols="5" sm="4" md="4"
+            color="grey lighten-3"
+            v-show="show.data"
+        >
+
+          <div
+              style="background-color: rgb(40,40,40); text-align: center; height: 100vh;">
+
+            <v-card-actions class="pr-5 pt-5">
+              <v-spacer></v-spacer>
+              <v-btn
+                  icon
+                  color="rgb(220,220,220)"
+                  @click.stop="show.data =false"
+              >
+                <v-icon size="40px">mdi-close</v-icon>
+              </v-btn>
+            </v-card-actions>
+
+            <div class="align-center d-flex flex-column pt-8">
+              <v-img
+                  :src="selectBook.bookThumb"
+                  style=" width: 200px"
+              ></v-img>
+
+              <h4 class="pt-4" style="color: rgb(220,220,220)"> {{selectBook.bookTitle}} </h4>
+
+              <v-card-subtitle style="color: rgb(220,220,220)">{{selectBook.bookAuthor}} | {{selectBook.bookPublisher}}</v-card-subtitle>
+
+              <v-chip-group
+                  active-class="primary--text"
+                  multiple
+                  column
+                  v-model="selection"
+              >
+                <v-chip
+                    class="inner-chip"
+                    v-for="keyword in keywords[1]"
+                    :key="keyword"
+                    :value="keyword"
+                    outlined
+                    color="rgb(220,220,220)"
+                    small
+                ><span>{{keyword}}</span>
+                </v-chip>
+              </v-chip-group>
+
+              <v-divider></v-divider>
+              <v-card-actions>
+                <v-tooltip top color="pink">
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                        v-bind="attrs"
+                        v-on="on"
+                        icon
+                    >
+                      <v-icon color="pink">
+                        mdi-heart
+                      </v-icon>
+                    </v-btn>
+                  </template>
+                  <span>위시리스트에 추가</span>
+                </v-tooltip>
+
+                <v-tooltip top color="green">
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                        v-bind="attrs"
+                        v-on="on"
+                        icon
+                    >
+                      <v-icon color="green">
+                        mdi-book-open-variant
+                      </v-icon>
+                    </v-btn>
+                  </template>
+                  <span>책 보러가기</span>
+                </v-tooltip>
+              </v-card-actions>
+            </div>
+          </div>
+        </v-col>
+      </transition>
+
+
+    </v-row>
+
+
+  </v-container>
 </template>
 
 <script>
-import rain from "@/assets/rain.mp4"
+
 export default {
   name: "exam",
-  components:[rain],
   data: () => ({
-    rain
+    bookDatas : [],
+    keywords : [],
+    selection : [],
+    selectTag : '',
+    show : {data:false , bid: null},
+
+    selectBook : '',
+
   }),
+
+
+  methods: {
+    getBookInfo(){
+      this.$axios.get('book/info')
+          .then(response=>{
+            this.bookDatas = response.data
+            for(let i =0; i<response.data.length; i++){
+              this.keywords.push(response.data[i].bookKeyword.split(','))
+            }
+          })
+          .catch(error =>{
+            console.log(error.response);
+          })
+    },
+
+
+    openInfo(book){
+      if(this.show.data === true){  //책 정보가 열려있을때
+
+        if(this.show.bid === book.bid){ //같은 책이라면 정보 닫기
+          this.show.data= !this.show.data
+        }
+
+      }else{  //닫혀있으면 열기
+        this.show.data= !this.show.data
+      }
+
+      this.show.bid = book.bid
+      this.selectBook=book
+    },
+
+
+  },
+  mounted() {
+    this.getBookInfo()
+  }
 }
 </script>
 
@@ -51,4 +199,32 @@ video {
   font-size: 48px;
   color: #ffffff;
 }
+.right-section{
+  display: block;
+  position: relative;
+}
+
+
+.sub-slide-enter{
+  transform: translateX(400px);
+  opacity: 1;
+}
+.sub-slide-enter-active,
+.sub-slide-leave-active {
+  transition: all 0.3s ease-out;
+}
+
+.sub-slide-leave-to {
+  transform: translateX(400px);
+  opacity: 1;
+}
+
+
+@media screen and (max-width: 768px){
+  .right-section{
+    display: none;
+    position: absolute;
+  }
+}
+
 </style>
